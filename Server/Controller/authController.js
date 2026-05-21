895e2fdc3171e82e5b87f0a79e443c7bd601ef3c
Stalwartech <Abdulsalamakintaro@gmail.com>
2026-05-21 17:31:36 +0100

    idjd

--- Server/Controller/authController.js
+++ Server/Controller/authController.js
@@ -1,7 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const env = require("dotenv").config();
-const mongoose = require("mongoose");
 // Import the auth model here 
 // const authModel = require()
 
